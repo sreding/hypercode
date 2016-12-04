058b9675a08be900fc5ee4842b4bb0c2709d01cc
sreding <redins@usi.ch>
2016-12-04 22:45:11 +0100

    completed draft layout for file container

--- frontend/src/components/file-container.vue
+++ frontend/src/components/file-container.vue
@@ -1,6 +1,14 @@
 <template>
-  <div id="file-container">
-    <textarea name="code" id="code"></textarea>
+  <div id="file-container" >
+  <div id="wrapper">
+  <a href="#" id="exit-btn">x</a>
+  <h1>{{title}}</h1>
+    <textarea name="code" id="code">{{text}}</textarea>
+    <a href="#" id="prev-btn">&lt</a>
+    <a href="#" id="next-btn">&gt</a>
+    <a href="#" id="save-btn">Save</a>
+
+  </div>
   </div>
 </template>
 
@@ -16,18 +24,99 @@ export default {
       mode: 'text/x-java',
     });
     this.cm.setSize(this.width, this.height);
+
+    this.container =  this.$el.querySelector('#wrapper');
+
+    this.container.style.width=this.width+"px";
+    this.container.style.height=100+this.height+"px"
+    
   },
   props: {
     'width': { default: 400 },
     'height': { default: 400 },
+    'code': {default:"hello world"}
   },
   data: function () {
     return {
       cm: null,
-      title: '',
+      title: 'fileName.java',
+      text: 'hello world'
     };
   }
 }
 </script>
 
-<style src="codemirror/lib/codemirror.css"></style>
+<style src="codemirror/lib/codemirror.css">
+
+</style>
+<style>
+#exit-btn{
+  background-color:#991f00;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:10px;
+  padding:10px 10px;
+  text-decoration:none;
+  position:absolute;
+  top:0px;
+  left:0px;
+}
+
+#save-btn{
+  background-color:#cc8800;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:21px;
+  padding:10px 10px;
+  text-decoration:none;
+  position:absolute;
+  bottom:0px;
+  right:25px;
+}
+
+#next-btn{
+  background-color:#cc8800;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:25px;
+  padding:15px 5px;
+  text-decoration:none;
+  position:absolute;
+  bottom:30%;
+  right:0px;
+}
+
+#prev-btn{
+  background-color:#cc8800;
+  display:inline-block;
+  cursor:pointer;
+  color:#ffffff;
+  font-family:Arial;
+  font-size:25px;
+  padding:15px 5px;
+  text-decoration:none;
+  position:absolute;
+  bottom:30%;
+  left:0px;
+}
+
+#wrapper{
+  background-color:#333333;
+  width:400px;
+  height: 550px;
+  margin:10px;
+  padding:25px;
+  position:relative;
+}
+h1{
+  color:white;
+  font-family:'Helvetica Neue', Helvetica, Arial;
+  font-weight:normal;
+  text-align:center;
+}
+</style>
