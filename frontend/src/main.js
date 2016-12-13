359ec973833f93eda9fe87104fcd7a2947da18b7
sreding <redins@usi.ch>
2016-12-13 16:09:44 +0100

    implemented front end routing WARNING: this requires npm install for vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,37 @@
 import Vue from 'vue';
 import App from './app.vue';
+import VueRouter from 'vue-router';
+import ContextView from './components/context-view.vue';
+import FileContainer from './components/file-container.vue';
 
-new Vue({
-  el: '#app',
+const Entry = {
+	template: '<p>/file for ContextView, /container for FileContainer</p>'
+}
+
+const NotFound = {
+	template: '<p>404 - not found</p>'
+}
+
+const routes = [
+   
+  //will eventually be replaced by overview
+  { path: '/container', component: FileContainer },
+  { path: '/file', component: ContextView },
+  { path: '/file/:id', component: ContextView },
+  { path: '/', component: Entry },
+  { path: '*', component: NotFound }
+  
+]
+
+const router = new VueRouter({
+	mode:'history',
+  routes // short for routes: routes
+})
+
+
+Vue.use(VueRouter);
+const app = new Vue({
+  el:'#app',
   render: h => h(App),
-});
+  router
+})
